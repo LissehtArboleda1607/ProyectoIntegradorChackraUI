f09376b1e9ae1554671dd6f7e30a4fe7cb0413d0
Lisseht Arboleda <licarboleda@gmail.com>
2023-12-15 20:51:30 -0500

    se agrego lo necesario en la rama Project-3 de acuerdo con lo requerido en Custom Hooks - 2

---
+++ my-react-task-list/src/Pages/SobreNosotros.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SobreNosotros = () => {
+  return (
+    <div>
+      <h2>Sobre Nosotros</h2>
+      <p>Descripción de la aplicación y sus funcionalidades.</p>
+    </div>
+  );
+};
+
+export default SobreNosotros;
---
+++ my-react-task-list/src/Pages/Tareas.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tareas = () => {
+  return (
+    <div>
+      <h2>Listado de Tareas</h2>
+      {/* Aquí puedes incluir tu lógica para mostrar el listado de tareas */}
+    </div>
+  );
+};
+
+export default Tareas;
